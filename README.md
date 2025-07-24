af828af504dbd21c2d8953533330ecbdec81b7bc
M16yusuf <yusufsmd58@gmail.com>
2025-07-25 01:25:08 +0700

    feat: add feature task no2, doc: add detail task week4day2 on readme

--- README.md
+++ README.md
@@ -2,40 +2,50 @@
 
 
 <details>
+<summary> Details Progress Minitask Week 4 Days 2 </summary>
+
+### Number 1 make hamburger menu
+
+![no_1](./demo/w4d2-no1.png)
+
+### Number 2 make validation 
+
+![no_2](./demo/w4d2-no2.png)
+
+
+
+
+
+</details>
+
+
+
+
+
+
+<details>
 <summary> Detail weekly task </summary>
 
 ### Tugas week 3
 
 Buatlah slicing halaman web dari mockup dibawah ini menggunakan HTML CSS [ticketing](https://www.figma.com/design/M930Xuv5WTzpLcJvqNovUF/Fazztrack---Tickitz?node-id=0-1&p=f&t=YBAyRP8nKI2cBr2y-0)
 
+```
 Requirements:
-
 1. HTML5 Semantik
-
 2. Grid/flexbox
-
 3. ExternalCSS
-
 4. Media query
-
 5. Responsive
-
 6. navigasi halaman
-
 7. Buat semirip mungkin sesuai figma
-
 8. Upload ke github
-
 9. Deploy ke vercel
-
 Penggunakan Bahasa Inggris untuk nama class atau id
-
 Upload/push tugas kamu ke github dan gunakan nama yang profesional
-
 Presentasikan apa yang telah Anda lakukan dalam minggu ini
-
 - Ceritakan tentang project yang kamu kerjakan
-
 - Jelaskan apa itu konsep tentang (HTML, CSS, Responsive Web)
+```
 
 </details>
---
+++ js/validasition.js
@@ -0,0 +1,61 @@
+const form = document.querySelector("form");
+const emailInput = form.querySelector("input#ele_mail");
+const passInput = form.querySelector("input#pass");
+
+// console.log(form);
+
+passInput.addEventListener("change", (er) => {
+    const passW = er.target.value;
+    const mustHvDigit = /\d/ ;
+    const mustHvChar = /^[a-zA-Z]+$/ ;
+    // !@#$%^&*/><
+    const mustHvSimbol = /[!@#$%^&*/><]/;
+
+    
+
+    if(mustHvDigit.test(passW) === false){
+        const p = document.createElement("p");
+        p.textContent = "❌ The password must contain numbers !";
+        p.style.cssText =  "font-size: 0.75rem; margin: 2px; color:red";
+        passInput.insertAdjacentElement("afterend", p);
+        console.log("mustHavedigit");
+    }
+    if(mustHvChar.test(passW)){
+        const p = document.createElement("p");
+        p.textContent = "❌ The password must contain characters !";
+        p.style.cssText =  "font-size: 0.75rem; margin: 2px; color:red";
+        passInput.insertAdjacentElement("afterend", p);
+        console.log("musthaveCharr");
+    }
+    if(mustHvSimbol.test(passW) === false){
+        const p = document.createElement("p");
+        p.textContent = "❌ The passwords must contain symbols ' !@#$%^&*/><' ";
+        p.style.cssText =  "font-size: 0.75rem; margin: 2px; color:red";
+        passInput.insertAdjacentElement("afterend", p);
+        console.log("musthavesimbol");
+    }
+    if(passW.length < 8 ){
+        const p = document.createElement("p");
+        p.textContent = "❌ The password must contain 8 characters";
+        p.style.cssText =  "font-size: 0.75rem; margin: 2px; color:red";
+        passInput.insertAdjacentElement("afterend", p);
+        console.log("must8char");
+    }
+    if(passW == " "){
+        const p = document.createElement("p");
+        p.textContent = "❌ pThe assword cannot be empty";
+        p.style.cssText =  "font-size: 0.75rem; margin: 2px; color:red";
+        passInput.insertAdjacentElement("afterend", p);
+        console.log("cammot empty")
+    }
+
+});
+
+form.addEventListener("submit", (e)  =>{
+    e.preventDefault();
+    const form = document.querySelector("form");
+    const getEmail = form.querySelector("input#ele_mail");
+    const getPass = form.querySelector("input#pass");
+    console.log(`this email ${getEmail.textContent} this pass ${getPass.textContent}`);
+});
+
